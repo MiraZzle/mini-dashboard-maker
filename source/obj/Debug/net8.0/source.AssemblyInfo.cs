a78ff479e8d989c9fe597665c0f87ddecc91d0ce
Matěj Foukal <matfou01@gmail.com>
2025-06-03 14:01:20 +0200

    chore: misc

--- source/obj/Debug/net8.0/source.AssemblyInfo.cs
+++ source/obj/Debug/net8.0/source.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("source")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b3f9d21dfe2988e6671f710251f1ee622d37b5bf")]
 [assembly: System.Reflection.AssemblyProductAttribute("source")]
 [assembly: System.Reflection.AssemblyTitleAttribute("source")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
