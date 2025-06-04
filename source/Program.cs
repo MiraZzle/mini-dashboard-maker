318dff386a29afdedb68d28cced984dbbf4c0d09
Matěj Foukal <matfou01@gmail.com>
2025-06-04 22:07:38 +0200

    feat: register data sources service

--- source/Program.cs
+++ source/Program.cs
@@ -1,4 +1,6 @@
 using source.Components;
+using source.Services.DataSources;
+
 
 namespace source
 {
@@ -11,12 +13,14 @@ namespace source
             builder.Services.AddRazorComponents()
                 .AddInteractiveServerComponents();
 
+            builder.Services.AddSingleton<DataSourceService>();
+
+
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
             if (!app.Environment.IsDevelopment()) {
                 app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
 
